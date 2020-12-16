52907070081a66871d4c2ef4696c4297368fc5cc
阿宝 <talent518@yeah.net>
2020-12-16 16:59:35 +0800

    添加函数share_var_get_and_del，修复arguments[0]相关代码，修复socket.php报socket_import_fd第一个不能为空问题。

--- func.c
+++ func.c
@@ -624,7 +624,7 @@ static PHP_FUNCTION(task_set_run) {
 static hash_table_t *share_var_ht = NULL;
 static pthread_mutex_t share_var_rlock;
 static pthread_mutex_t share_var_wlock;
-static int share_var_locks = 0;
+static volatile int share_var_locks = 0;
 
 ZEND_BEGIN_ARG_INFO_EX(arginfo_share_var_init, 0, 0, 0)
 ZEND_ARG_TYPE_INFO(0, size, IS_LONG, 0)
@@ -635,6 +635,10 @@ ZEND_ARG_VARIADIC_INFO(0, keys)
 ZEND_END_ARG_INFO()
 
 ZEND_BEGIN_ARG_INFO_EX(arginfo_share_var_get, 0, 0, 0)
+ZEND_ARG_VARIADIC_INFO(0, keys)
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_INFO_EX(arginfo_share_var_get_and_del, 0, 0, 0)
 ZEND_ARG_VARIADIC_INFO(0, keys)
 ZEND_END_ARG_INFO()
 
@@ -781,7 +785,7 @@ static PHP_FUNCTION(share_var_exists)
 		} else if(Z_TYPE(arguments[i]) == IS_LONG) {
 			if(hash_table_index_find((hash_table_t*) v1.ptr, Z_LVAL(arguments[i]), &v2) == FAILURE) break;
 		} else {
-			convert_to_string(&arguments[0]);
+			convert_to_string(&arguments[i]);
 			if(hash_table_find((hash_table_t*) v1.ptr, Z_STRVAL(arguments[i]), Z_STRLEN(arguments[i]), &v2) == FAILURE) break;
 		}
 		v1 = v2;
@@ -955,13 +959,60 @@ static PHP_FUNCTION(share_var_get)
 		if(Z_TYPE(arguments[i]) == IS_LONG) {
 			if(hash_table_index_find((hash_table_t*) v1.ptr, Z_LVAL(arguments[i]), &v2) == FAILURE) break;
 		} else {
-			convert_to_string(&arguments[0]);
+			convert_to_string(&arguments[i]);
 			if(hash_table_find((hash_table_t*) v1.ptr, Z_STRVAL(arguments[i]), Z_STRLEN(arguments[i]), &v2) == FAILURE) break;
 		}
 		if(i == arg_num - 1) value_to_zval(&v2, return_value);
 		else v1 = v2;
 	}
 	SHARE_VAR_RUNLOCK();
+
+	end:
+	efree(arguments);
+}
+
+static PHP_FUNCTION(share_var_get_and_del)
+{
+	zval *arguments;
+	int arg_num = ZEND_NUM_ARGS(), i;
+
+	if(!share_var_ht) return;
+
+	if(arg_num <= 0) {
+		SHARE_VAR_WLOCK();
+		array_init_size(return_value, hash_table_num_elements(share_var_ht));
+		hash_table_apply_with_argument(share_var_ht, (hash_apply_func_arg_t) hash_table_to_zval, return_value);
+		hash_table_clean(share_var_ht);
+		SHARE_VAR_WUNLOCK();
+		return;
+	}
+
+	arguments = (zval *) safe_emalloc(sizeof(zval), arg_num, 0);
+	if(zend_get_parameters_array_ex(arg_num, arguments) == FAILURE) goto end;
+
+	SHARE_VAR_WLOCK();
+	value_t v1 = {.type=HT_T,.ptr=share_var_ht,.expire=0}, v2 = {.type=NULL_T,.expire=0};
+	for(i=0; i<arg_num && v1.type == HT_T; i++) {
+		if(Z_TYPE(arguments[i]) == IS_LONG) {
+			if(hash_table_index_find((hash_table_t*) v1.ptr, Z_LVAL(arguments[i]), &v2) == FAILURE) break;
+
+			if(i == arg_num - 1) {
+				value_to_zval(&v2, return_value);
+				hash_table_index_del((hash_table_t*) v1.ptr, Z_LVAL(arguments[i]));
+			}
+		} else {
+			convert_to_string(&arguments[i]);
+			if(hash_table_find((hash_table_t*) v1.ptr, Z_STRVAL(arguments[i]), Z_STRLEN(arguments[i]), &v2) == FAILURE) break;
+			
+			if(i == arg_num - 1) {
+				value_to_zval(&v2, return_value);
+				hash_table_del((hash_table_t*) v1.ptr, Z_STRVAL(arguments[i]), Z_STRLEN(arguments[i]));
+			}
+		}
+
+		v1 = v2;
+	}
+	SHARE_VAR_WUNLOCK(); // if(i!=arg_num) {printf("TYPE: %d,%d,%d\n", v1.type, v2.type, i);php_var_dump(&arguments[0], 0);php_var_dump(&arguments[i], 0);}
 
 	end:
 	efree(arguments);
@@ -1117,7 +1168,7 @@ static PHP_FUNCTION(share_var_put)
 					}
 				}
 			} else {
-				convert_to_string(&arguments[0]);
+				convert_to_string(&arguments[i]);
 				if(hash_table_find((hash_table_t*) v1.ptr, Z_STRVAL(arguments[i]), Z_STRLEN(arguments[i]), &v2) == FAILURE) {
 					v2.type = HT_T;
 					v2.ptr = malloc(sizeof(hash_table_t));
@@ -1286,7 +1337,7 @@ static PHP_FUNCTION(share_var_inc)
 					}
 				}
 			} else {
-				convert_to_string(&arguments[0]);
+				convert_to_string(&arguments[i]);
 				if(hash_table_find((hash_table_t*) v1.ptr, Z_STRVAL(arguments[i]), Z_STRLEN(arguments[i]), &v2) == FAILURE) {
 					v2.type = HT_T;
 					v2.ptr = malloc(sizeof(hash_table_t));
@@ -1347,7 +1398,7 @@ static PHP_FUNCTION(share_var_set)
 				}
 			}
 		} else {
-			convert_to_string(&arguments[0]);
+			convert_to_string(&arguments[i]);
 			if(hash_table_find((hash_table_t*) v1.ptr, Z_STRVAL(arguments[i]), Z_STRLEN(arguments[i]), &v2) == FAILURE) {
 				v2.type = HT_T;
 				v2.ptr = malloc(sizeof(hash_table_t));
@@ -1408,7 +1459,7 @@ static PHP_FUNCTION(share_var_set_ex)
 				}
 			}
 		} else {
-			convert_to_string(&arguments[0]);
+			convert_to_string(&arguments[i]);
 			if(hash_table_find((hash_table_t*) v1.ptr, Z_STRVAL(arguments[i]), Z_STRLEN(arguments[i]), &v2) == FAILURE) {
 				v2.type = HT_T;
 				v2.ptr = malloc(sizeof(hash_table_t));
@@ -1455,7 +1506,7 @@ static PHP_FUNCTION(share_var_del)
 		} else if(Z_TYPE(arguments[i]) == IS_LONG) {
 			if(hash_table_index_find((hash_table_t*) v1.ptr, Z_LVAL(arguments[i]), &v2) == FAILURE) break;
 		} else {
-			convert_to_string(&arguments[0]);
+			convert_to_string(&arguments[i]);
 			if(hash_table_find((hash_table_t*) v1.ptr, Z_STRVAL(arguments[i]), Z_STRLEN(arguments[i]), &v2) == FAILURE) break;
 		}
 		v1 = v2;
@@ -1535,7 +1586,7 @@ static PHP_FUNCTION(share_var_count)
 		if(Z_TYPE(arguments[i]) == IS_LONG) {
 			if(hash_table_index_find((hash_table_t*) v1.ptr, Z_LVAL(arguments[i]), &v2) == FAILURE) break;
 		} else {
-			convert_to_string(&arguments[0]);
+			convert_to_string(&arguments[i]);
 			if(hash_table_find((hash_table_t*) v1.ptr, Z_STRVAL(arguments[i]), Z_STRLEN(arguments[i]), &v2) == FAILURE) break;
 		}
 		if(i == arg_num - 1) {
@@ -1726,6 +1777,7 @@ static const zend_function_entry ext_functions[] = {
 	PHP_FE(share_var_init, arginfo_share_var_init)
 	PHP_FE(share_var_exists, arginfo_share_var_exists)
 	PHP_FE(share_var_get, arginfo_share_var_get)
+	PHP_FE(share_var_get_and_del, arginfo_share_var_get_and_del)
 	PHP_FE(share_var_put, arginfo_share_var_put)
 	PHP_FE(share_var_inc, arginfo_share_var_inc)
 	PHP_FE(share_var_set, arginfo_share_var_set)
---
+++ socket-client.php
@@ -0,0 +1,72 @@
+<?php
+$running = true;
+$exitSig = 0;
+function signal($sig) {
+	global $running;
+
+	$exitSig = $sig;
+	$running = false;
+	defined('THREAD_TASK_NAME') or task_set_run(false);
+}
+
+pcntl_async_signals(true);
+pcntl_signal(SIGTERM, 'signal', false);
+pcntl_signal(SIGINT, 'signal', false);
+
+if(defined('THREAD_TASK_NAME')) {
+	$host = $_SERVER['argv'][1];
+	$port = (int) $_SERVER['argv'][2];
+	while($running) {
+		$fd = @socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+		if($fd === false) {
+			share_var_inc('errs', 1);
+			//strerror('socket_connect', false);
+			continue;
+		}
+		if(!@socket_connect($fd, $host, $port)) {
+			share_var_inc('errs', 1);
+			//strerror('socket_connect', false);
+			@socket_close($fd);
+			continue;
+		}
+		if(($str = @socket_read($fd, 1024)) !== false) {
+			share_var_inc('conns', 1);
+			//echo $str;
+			@socket_write($fd, '1');
+		}
+		@socket_close($fd);
+	}
+} else {
+	$host = ($_SERVER['argv'][1]??'127.0.0.1');
+	$port = (int) ($_SERVER['argv'][2]??5000);
+	$conns = (int) ($_SERVER['argv'][3]??100);
+	
+	task_set_threads($conns);
+	share_var_init(2);
+	for($i=0; $i<$conns; $i++) create_task('conn' . $i, __FILE__, [$host, $port]);
+	$n = 0;
+	$e = 0;
+	while($running) {
+		sleep(1);
+		$n2 = share_var_get('conns');
+		$n3 = share_var_get('errs');
+		$n = $n2 - $n;
+		$e = $n3 - $e;
+		echo "$n connects, $e errors\n";
+		$n = $n2;
+		$e = $n3;
+	}
+	
+	task_wait($exitSig?:SIGINT);
+	share_var_destory();
+	
+	echo "Stoped\n";
+}
+	
+function strerror($msg, $isExit = true) {
+	$err = socket_last_error();
+	printf("[%s] %s(%d): %s\n", defined('THREAD_TASK_NAME') ? THREAD_TASK_NAME : 'main', $msg, $err, socket_strerror($err));
+
+	if($isExit) exit; else return true;
+}
+
